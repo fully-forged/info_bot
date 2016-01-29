07a89dddc0e542c89bc375ad20037caf5555bed1
Claudio Ortolina <claudio@fullyforged.com>
2016-01-29 16:01:45 +0000

    Adds function to get list of upcoming bus data

--- info_bot.d
+++ info_bot.d
@@ -1,3 +1,4 @@
+src/info_bot_bus.erl:: src/info_bot_bus.hrl; @touch $@
 src/info_bot_device_store.erl:: src/info_bot_device.hrl; @touch $@
 src/info_bot_particle_events_handler.erl:: src/sse_event.hrl; @touch $@
 src/info_bot_tfl_api.erl:: src/info_bot_bus.hrl; @touch $@
